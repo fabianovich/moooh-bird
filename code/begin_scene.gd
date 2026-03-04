d159e60ca3fbffc030b7aa04e449fb1d677d5841
fabianovich <fabianvanacoley@gmail.com>
2026-03-04 22:17:44 +0100

    dialogue

--- code/begin_scene.gd
+++ code/begin_scene.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
-var lines = [["I ducking hate cows!!", "All they do is fart and eat grass all day!!", "Milk and beef don't even taste that good!!"], 
-			["What if I got rid of them?", "It's not like people are gonna miss them", "Only, how do I do that?"], 
-			["Ah hah!", "What if I poisoned their grass?", "..."] ]
+var lines = ["I ducking hate cows!!", "All they do is fart and eat grass all day!!", "Milk and beef don't even taste that good!!", 
+			"What if I got rid of them?", "It's not like people are gonna miss them", "Only, how do I do that?", 
+			"Ah hah!", "What if I poisoned their grass?", "..."]
 @onready var label: RichTextLabel = $dialog
 
 
 var dt = 0
 var bar = 0
-var display = ""
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if bar == 0 or (Input.is_action_pressed("space") and bar < 4):
-		for line in lines[bar]:
-			typewriter(line, 50.0)
-			
+	if Input.is_action_just_pressed("space") or bar == 0:
+		if bar < lines.size():
+			typewriter(lines[bar], 50.0)
+			bar += 1
+		else:
+			proceed()
 	
 
 
@@ -25,3 +26,6 @@ func typewriter(text: String, chars_per_second: float = 20.0) -> void:
 	var tween = create_tween()
 	tween.tween_property(label, "visible_characters", label.get_total_character_count(), 
 						 label.get_total_character_count() / chars_per_second)
+						
+func proceed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
--- code/startscreen.gd
+++ code/startscreen.gd
@@ -4,5 +4,5 @@ extends Control
 func _process(_delta: float) -> void:
 
 	if Input.is_action_pressed("g"):
-		get_tree().change_scene_to_file("res://scenes/game.tscn")
+		get_tree().change_scene_to_file("res://scenes/begin_scene.tscn")
 		
